590bb89fa6bcba33497dc7004258a5788bf7e582
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-10-31 03:27:26 +0100

    Ionic, Angular, Tailwind and component & page setup

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { HeroComponent } from 'src/app/components/home/hero/hero/hero.component';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage,
+    HeroComponent]
+})
+export class HomePageModule {}
