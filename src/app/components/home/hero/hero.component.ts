d157aafc71b08d870dc45e77248fb96385771658
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-10-31 04:15:33 +0100

    changed component selector for  hero home

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'home-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
 })
