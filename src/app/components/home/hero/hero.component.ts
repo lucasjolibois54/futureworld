61d68d260326a45ab57d4eddc75ec1a9735c974c
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 13:35:20 +0100

    data parent to child setup

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'home-hero',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+  @Input() hero
   constructor() { }
 
   ngOnInit() {}
