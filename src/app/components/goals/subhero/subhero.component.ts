ba98d12137c7457ebe49381467870415ae58b3bb
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 23:15:15 +0100

    subhero component

---
+++ src/app/components/goals/subhero/subhero.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subhero',
+  templateUrl: './subhero.component.html',
+  styleUrls: ['./subhero.component.scss'],
+})
+export class SubheroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
