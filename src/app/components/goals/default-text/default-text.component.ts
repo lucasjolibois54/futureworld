fbe42fa2086d7154808cb1718f30a44c66dd341c
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 23:17:21 +0100

    default text component

---
+++ src/app/components/goals/default-text/default-text.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-text',
+  templateUrl: './default-text.component.html',
+  styleUrls: ['./default-text.component.scss'],
+})
+export class DefaultTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
