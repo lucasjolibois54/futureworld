3398d86ce9f8fdec6e8b748a16ee10cf0dd4d614
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 15:07:43 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,56 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    screens: {
+      'xxsm': '355px',
+      // => @media (min-width: 355px) { ... }
+
+      'xsm': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'semixsm': '408px',
+      // => @media (min-width: 408px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'md-blog-img': '791px',
+      // => @media (min-width: 791px) { ... }
+
+      'lg-blog-img': '839px',
+      // => @media (min-width: 839px) { ... }
+
+      'md-lg': '878px',
+      // => @media (min-width: 878px) { ... }
+
+      'md-lg-bigger': '915px',
+      // => @media (min-width: 915px) { ... }
+
+      'sm-lg': '696px',
+      // => @media (min-width: 696px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'biggerlg': '1039px',
+      // => @media (min-width: 1039px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        // colors
+        'blue-hover': '#00ddff',
+        'white-button': '#D8D8D8',
+      }
+    },
   },
   plugins: [],
 };
