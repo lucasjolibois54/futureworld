6aac44b16768699e61bf61f2fe0405e3271fe2ff
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 23:17:05 +0100

    fact box component

---
+++ src/app/components/goals/fact-box/fact-box.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fact-box',
+  templateUrl: './fact-box.component.html',
+  styleUrls: ['./fact-box.component.scss'],
+})
+export class FactBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
