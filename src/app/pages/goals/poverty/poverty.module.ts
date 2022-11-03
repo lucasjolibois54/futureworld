c8076ebc11abd3e90535d2a00a024a2110549e36
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 23:56:17 +0100

    Update poverty.module.ts

--- src/app/pages/goals/poverty/poverty.module.ts
+++ src/app/pages/goals/poverty/poverty.module.ts
@@ -7,6 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { PovertyPageRoutingModule } from './poverty-routing.module';
 
 import { PovertyPage } from './poverty.page';
+import { NavComponent } from 'src/app/components/main/nav/nav.component';
+import { HeroComponent } from 'src/app/components/goals/hero/hero.component';
+import { SubheroComponent } from 'src/app/components/goals/subhero/subhero.component';
+import { DefaultTextComponent } from 'src/app/components/goals/default-text/default-text.component';
+import { VideoComponent } from 'src/app/components/goals/video/video.component';
+import { FactBoxComponent } from 'src/app/components/goals/fact-box/fact-box.component';
+import { ImageComponent } from 'src/app/components/goals/image/image.component';
+import { QuoteComponent } from 'src/app/components/goals/quote/quote.component';
+import { NextProjectComponent } from 'src/app/components/goals/next-project/next-project.component';
+import { FooterComponent } from 'src/app/components/main/footer/footer.component';
 
 @NgModule({
   imports: [
@@ -15,6 +25,17 @@ import { PovertyPage } from './poverty.page';
     IonicModule,
     PovertyPageRoutingModule
   ],
-  declarations: [PovertyPage]
+  declarations: [PovertyPage,
+    NavComponent,
+    HeroComponent,
+    SubheroComponent,
+    DefaultTextComponent,
+    VideoComponent,
+    FactBoxComponent,
+    ImageComponent,
+    QuoteComponent,  
+    NextProjectComponent,  
+    FooterComponent
+  ]
 })
 export class PovertyPageModule {}
