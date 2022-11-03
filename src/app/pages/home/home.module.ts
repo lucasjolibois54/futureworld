d64094a7e39a3e3243e4598fd720b698d67707ac
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-03 14:18:29 +0100

    navigation import

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 import { HeroComponent } from 'src/app/components/home/hero/hero.component';
+import { NavComponent } from 'src/app/components/main/nav/nav.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -16,6 +17,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     HomePageRoutingModule
   ],
   declarations: [HomePage,
+    NavComponent,
     HeroComponent]
 })
 export class HomePageModule {}
