6121774ec591e46b3b59b9d433a3efd4100b131b
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-04 13:40:33 +0100

    secondary text component

---
+++ src/app/components/goals/secondary-default-text/secondary-default-text.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-default-text',
+  templateUrl: './secondary-default-text.component.html',
+  styleUrls: ['./secondary-default-text.component.scss'],
+})
+export class SecondaryDefaultTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
