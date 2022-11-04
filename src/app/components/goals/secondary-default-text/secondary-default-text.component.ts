d58b1e52d36a0cb7802753982c5cd632911e7adc
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-04 14:55:21 +0100

    Update secondary-default-text.component.ts

--- src/app/components/goals/secondary-default-text/secondary-default-text.component.ts
+++ src/app/components/goals/secondary-default-text/secondary-default-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-default-text',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./secondary-default-text.component.scss'],
 })
 export class SecondaryDefaultTextComponent implements OnInit {
+
+  @Input() secondarytext
 
   constructor() { }
 
