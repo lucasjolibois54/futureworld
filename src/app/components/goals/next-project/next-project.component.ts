45aca5be17e5f25204c8eb43cfd4df6d68f8e4ad
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-04 15:55:45 +0100

    Update next-project.component.ts

--- src/app/components/goals/next-project/next-project.component.ts
+++ src/app/components/goals/next-project/next-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-next-project',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./next-project.component.scss'],
 })
 export class NextProjectComponent implements OnInit {
+
+  @Input() nextproject
 
   constructor() { }
 
