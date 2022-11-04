4e1619d52f2b4c8dbed325926eee89e5f02f3fc7
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-04 14:55:17 +0100

    Update poverty.page.ts

--- src/app/pages/goals/poverty/poverty.page.ts
+++ src/app/pages/goals/poverty/poverty.page.ts
@@ -26,7 +26,37 @@ export class PovertyPage implements OnInit {
     textthree: '“Our goal is to help you as a student become less economical worried by giving you access to cheap & modern student apartments. We want to improve your consumption in the household and show you how to save money.',
   }
 
-  
+  secondarytextdata={
+    titlepartone: 'All of',
+    titlepartto: 'our',
+    titlepartthree: 'Sustainable',
+
+    textone: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.",
+    texttwo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.",
+    cta: "Our goals and values",
+    textthree: '“Our goal is to help you as a student become less economical worried by giving you access to cheap & modern student apartments. We want to improve your consumption in the household and show you how to save money.',
+  }
+
+  factboxdata={
+    titlepartone: 'Did you',
+    titlepartto: 'know',
+    titlepartthree: 'these',
+    titlepartfour: 'numbers?',
+
+    subtitleone: "More than",
+    subtitletwo: "More than",
+    subtitlethree: "More than",
+    subtitlefour: 'More than',
+
+    textone: "750mil in poverty",
+    texttwo: "600mil homeless",
+    textthree: "750mil in poverty",
+  }
+
+  quotedata={
+    textquote: '“Our goal is to help you as a student become less economical worried by giving you access to cheap & modern student apartments. We want to improve your consumption in the household and show you how to save money.',
+  }
+
 
   constructor() { }
 
