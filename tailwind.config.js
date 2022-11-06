1fc7c5aa24634bc3bb0da5d9946428d311695ad6
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-06 23:49:28 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
+    maxWidth: {
+      '0': '0px',
+      'none': 'none',
+      'xs': '20rem',
+      'sm': '24rem',
+      'md': '28rem',
+      'lg': '32rem',
+      'xl': '36rem',
+      '2xl': '42rem',
+      '3xl': '48rem',
+      '4xl': '56rem',
+      '5xl': '64rem',
+      '6xl': '72rem',
+      '7xl': '80rem',
+      'full': '100%',
+      'min': 'min-content',
+      'max': 'max-content',
+      'prose': '65ch',
+      'screen-sm': '640px',
+      'screen-md': '768px',
+      'screen-lg': '1024px',
+      'screen-xl': '1280px',
+      'screen-2xl': '1536px;',
+      '8xl': '2300px',
+
+    },
     screens: {
       'xxsm': '355px',
       // => @media (min-width: 355px) { ... }
@@ -59,6 +85,12 @@ module.exports = {
 
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
+
+      
+      '3xl': '1979px',
+      // => @media (min-width: 1536px) { ... }
+
+
     },
     extend: {
       colors: {
