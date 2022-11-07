c78e495c7daf66571bb1b4474cce9764bfa949ea
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-08 00:09:21 +0100

    Update four-goals.component.ts

--- src/app/components/home/four-goals/four-goals.component.ts
+++ src/app/components/home/four-goals/four-goals.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FourGoalsComponent implements OnInit {
 
+  fullscreen(video){
+    video.requestFullscreen();
+
+}
+
   constructor() { }
 
   ngOnInit() {}
