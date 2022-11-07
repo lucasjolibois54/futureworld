f14a6cc365052958947574509702d4faed730105
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-11-07 07:25:59 +0100

    Update home.page.ts

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomePage {
 
