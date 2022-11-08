76b9699ef104375dd79026df1d77349dce4e308d
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-08 18:30:28 +0100

    Create default-list-with-text.component.ts

---
+++ src/app/components/goals/default-list-with-text/default-list-with-text.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-list-with-text',
+  templateUrl: './default-list-with-text.component.html',
+  styleUrls: ['./default-list-with-text.component.scss'],
+})
+export class DefaultListWithTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
