be020762e9ed364d0afd38307fc326dfe1cd8375
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-11-08 18:17:35 +0100

    Update poverty.page.ts

--- src/app/pages/goals/poverty/poverty.page.ts
+++ src/app/pages/goals/poverty/poverty.page.ts
@@ -62,7 +62,7 @@ export class PovertyPage implements OnInit {
     texttwo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.",
     cta: "Our goals and values",
     ctalink: 'https://www.pexels.com/da-dk/video/huse-stov-fattig-faellesskab-3695933/',
-    textthree: '“Our goal is to help you as a student become less economical worried by giving you access to cheap & modern student apartments. We want to improve your consumption in the household and show you how to save money.',
+    textthree: '“Goal number 1 commits every nation of the world to initiate a targeted effort to increase the poor nations access to basic resources, like food, medicine, and service provisions like education.',
   }
 
   videodata={
@@ -102,7 +102,7 @@ export class PovertyPage implements OnInit {
   }
 
   quotedata={
-    textquote: '“Our goal is to help you as a student become less economical worried by giving you access to cheap & modern student apartments. We want to improve your consumption in the household and show you how to save money.',
+    textquote: '““Eradicating poverty is not a task of charity, it’s an act of justice and the key to unlocking an enormous human potential. Still, nearly half of the world’s population lives in poverty, and lack of food and clean water is killing thousands every single day of the year. Together, we can feed the hungry, wipe out disease and give everyone in the world a chance to prosper and live a productive and rich life.”    ',
   }
 
   nextprojectdata={
