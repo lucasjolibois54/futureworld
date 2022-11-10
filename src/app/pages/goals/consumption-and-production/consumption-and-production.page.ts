ece90dd539646b19de52ce88061bbdc9cf962485
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-10 19:30:50 +0100

    Revert "Commit"
    This reverts commit 52ac5707d80eefebfc97b46e2986a00266ddb8bf.

--- src/app/pages/goals/consumption-and-production/consumption-and-production.page.ts
+++ src/app/pages/goals/consumption-and-production/consumption-and-production.page.ts
@@ -53,9 +53,9 @@ export class ConsumptionAndProductionPage implements OnInit {
   }
 
   primarytextdata={
-    titlepartone: 'Use',
-    titleparttwo: 'recources',
-    titlepartthree: 'responsible',
+    titlepartone: 'All of',
+    titleparttwo: 'our',
+    titlepartthree: 'Sustainable',
 
     textone: "Our overuse of resources such as plastic, concrete, electronics etc. will quite naturally hit a wall in its current form. We will probably experience that material prices will generally rise and that more resources will become decidedly scarce.",
     texttwo: "The 12th world goal obligates countries to work together to ensure that we do not exploit our natural resources and that both the private individuals and common industrial dispose of toxic waste such as chemicals and plastics in a responsible manner.",
@@ -81,9 +81,9 @@ export class ConsumptionAndProductionPage implements OnInit {
   }
 
   primarylistdata={
-    titlepartone: 'Facts & Ideas',
-    titleparttwo: '',
-    titlepartthree: '',
+    titlepartone: 'All of',
+    titleparttwo: 'our',
+    titlepartthree: 'Sustainable',
 
     textone: "Facts",
     listone: "Be conscious of packaging – the less the better!",
